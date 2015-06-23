e4f38bbfb57ea1263172db450dd9b8a46a4315f7
hehe19900512 <870347271@qq.com>
2015-06-23 10:24:21 +0800

    Initial Commit

---
+++ PhotoAssets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoAssets
+//
+//  Created by yanshu on 15/6/23.
+//  Copyright (c) 2015年 yanshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoAssets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoAssets
+//
+//  Created by yanshu on 15/6/23.
+//  Copyright (c) 2015年 yanshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
